bb138676d13db6df64f903472a3d71f52f63003c
Alicja Szwalek <93785980+aszwalek1@users.noreply.github.com>
2024-07-29 15:08:09 +0100

    adds the readme

---
+++ README.cmd
@@ -0,0 +1,26 @@
+# Connect4
+## Technologies Used
+- **Java 11**: The main programming language for this application
+- **Gradle**: For managing of dependencies
+- **JUnit**: Testing framework for unit tests
+- **Java Swing (Future Integration)**: Option to integrate a Java Swing GUI in the future
+
+
+## Install and Run
+To clone the repository:
+```
+git clone https://github.com/aszwalek1/Connect4.git
+```
+
+
+### Running the application
+run this command to run the application in the command line
+```
+java -cp app/build/libs/app.jar projects.Connect4
+```
+
+### Running the tests
+run this command to run the tests
+```
+.\gradlew.bat test
+```

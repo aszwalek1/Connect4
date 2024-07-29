edd855a51497e59c0a13a9c0fe0e3cf4fb42f016
Alicja Szwalek <93785980+aszwalek1@users.noreply.github.com>
2024-07-29 11:45:36 +0100

    initialises the grid

--- app/src/main/java/projects/Connect4.java
+++ app/src/main/java/projects/Connect4.java
@@ -6,12 +6,10 @@ package projects;
 import java.util.Scanner;
 
 public class Connect4 {
-    public String initialiseGrid() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Connect4 game = new Connect4();
         
         // Asks user for the board size and winning condition
         System.out.println("Enter the row length: ");
@@ -20,5 +18,19 @@ public class Connect4 {
         int gridY = scanner.nextInt();
         System.out.println("Enter the number of tokens required to win: ");
         int winLength = scanner.nextInt();
+
+        game.initialiseGrid(gridX, gridY);
+
+        scanner.close();
+    }
+
+    public void initialiseGrid(int rows, int columns) {
+        char[][] grid = new char[rows][columns];
+
+        for (int row = 0; row < rows; row++) {
+            for (int column = 0; column < columns; column++) {
+                grid[row][column] = ' '; 
+            }
+        }
     }
 }
